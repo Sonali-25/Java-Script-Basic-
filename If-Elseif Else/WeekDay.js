4b7f22ffe329f48388a53d67184c6c8cb62889ab
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-28 16:40:13 +0530

    Find Days

---
+++ If-Elseif Else/WeekDay.js
@@ -0,0 +1,18 @@
+const Day = prompt("Enter The Number Between 1 To 7");
+if (Day == 1){
+    console.log("Day Is Monday")
+}else if(Day == 2){
+    console.log("Day Is Tuesday")
+}else if(Day == 3){
+    console.log("Day Is Wednesday")
+}else if(Day == 4){
+    console.log("Day Is Thursday")
+}else if(Day == 5){
+    console.log("Day is Friday")
+}else if(Day == 6){
+    console.log("Day Is Saturday")
+}else if(Day == 7){
+    console.log("Day Is Sunday")
+}else{
+    console.log("Please Enter The Number Between 1 To 7.")
+}
